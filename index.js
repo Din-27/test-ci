3d55d3ab12b2726227562bf01f3219cf3c8fecf0
herdin27 <it06.knitto@gmail.com>
2024-02-07 09:22:01 +0700

    update the response

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => res.send('hello'))
+app.get('/', (req, res) => res.send('hello nama saya herdiyana'))
 
 app.listen(PORT, () => console.log(`running on ${PORT}`))
